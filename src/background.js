4a6b4a1497f9ce6b10b064ca67becea3edbce3a8
metatrading <siroikutu@gmail.com>
2022-05-04 18:42:20 +0900

    ver 0.0.0.1

--- src/background.js
+++ src/background.js
@@ -1,8 +1,4 @@
-let color = '#3aa757';
-
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({color});
-    console.log('Default background color set to %cgreen', `color: ${color}`);
     chrome.contextMenus.create(
         {
             "id": "tweetFilter",
