1010b9468754fb5ac15a329628d9ccf8ae06364d
metatrading <siroikutu@gmail.com>
2022-05-04 17:44:36 +0900

    ver 0.0.0.1

---
+++ src/background.js
@@ -0,0 +1,24 @@
+let color = '#3aa757';
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({color});
+    console.log('Default background color set to %cgreen', `color: ${color}`);
+    chrome.contextMenus.create(
+        {
+            "id": "tweetFilter",
+            "contexts": ["page"],
+            "documentUrlPatterns": ["https://twitter.com/*"],
+            "title": "この人のツイートを表示する。"
+        }
+    )
+    const func = (info, tab) => {
+        console.log("context menu clicked.")
+        const targetId = info.pageUrl.split("/")[3]
+        const url = `https://twitter.com/search?q=from%3A${targetId}&src=typed_query&f=live`
+        console.log(`, targetId:${targetId} , target url :${url}`)
+        chrome.tabs.update(tab.id, {
+            url: url
+        })
+    }
+    chrome.contextMenus.onClicked.addListener(func)
+});
