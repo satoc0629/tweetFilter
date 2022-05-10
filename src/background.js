205330d3993d9a768cd158ab46150f844494e758
metatrading <S@toshi01>
2022-05-10 23:51:07 +0900

    update 0.0.0.2

--- src/background.js
+++ src/background.js
@@ -1,25 +1,38 @@
 const backgroundService = () => {
     chrome.contextMenus.create(
         {
-            "id": "tweetFilter",
-            "contexts": ["page"],
+            "id": "fromTweet",
+            "contexts": ["all"],
             "documentUrlPatterns": ["https://twitter.com/*"],
-            "title": "この人のツイートを表示する。"
+            "title": "From この人のツイートを表示する。"
         }
     )
-    const func = (info, tab) => {
-        if (info.menuItemId === "tweetFilter") {
-            console.log("context menu clicked.")
-            const targetId = info.pageUrl.split("/")[3]
-            const url = `https://twitter.com/search?q=from%3A${targetId}&src=typed_query&f=live`
-            console.log(`, targetId:${targetId} , target url :${url}`)
-            chrome.tabs.update(tab.id, {
-                url: url
-            })
+    chrome.contextMenus.create(
+        {
+            "id": "toTweet",
+            "contexts": ["all"],
+            "documentUrlPatterns": ["https://twitter.com/*"],
+            "title": "To この人へのツイートを表示する。"
         }
-    }
-    chrome.contextMenus.onClicked.addListener(func)
+    )
 };
+const func = (info, tab) => {
+    if (info.menuItemId === "fromTweet") {
+        const targetId = info.pageUrl.split("/")[3]
+        const url = `https://twitter.com/search?q=from%3A${targetId}&src=typed_query&f=live`
+        chrome.tabs.update(tab.id, {
+            url: url
+        })
+    }
+    if (info.menuItemId === "toTweet") {
+        const targetId = info.pageUrl.split("/")[3]
+        const url = `https://twitter.com/search?q=to%3A${targetId}&src=typed_query&f=live`
+        chrome.tabs.update(tab.id, {
+            url: url
+        })
+    }
+}
+chrome.contextMenus.onClicked.addListener(func)
 
 chrome.runtime.onInstalled.addListener(backgroundService);
 chrome.runtime.onStartup.addListener(backgroundService);
